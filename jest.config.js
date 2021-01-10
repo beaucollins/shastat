8161c30d3949ea0d53e6a57013f260bd830aaa5e
Beau Collins <beau@collins.pub>
2021-01-10 14:51:08 -0800

    prettier --fix

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!**/__tests__/**", "!**/node_modules/**", "!**/vendor/**"],
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testPathIgnorePatterns: ["dist"],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!**/__tests__/**', '!**/node_modules/**', '!**/vendor/**'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['dist'],
   // To be a test file, you need to be named foo.test.ts AND be in
   // a __tests__ directory.  This means you don't have to think about
   // where to put tests, and you can have non .test.ts helper files
   // live next to your tests.
-  testMatch: ["**/__tests__/**/(*.)+(spec|test).[jt]s?(x)"],
+  testMatch: ['**/__tests__/**/(*.)+(spec|test).[jt]s?(x)'],
 };
