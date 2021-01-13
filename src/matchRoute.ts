3038a93e61b70aae9683ee5bd8dfe6707ad39740
Beau Collins <beau@collins.pub>
2021-01-13 12:58:56 -0800

    improve types for matchRoute

--- src/matchRoute.ts
+++ src/matchRoute.ts
@@ -1,20 +1,20 @@
-import { Route } from '@fracture/serve';
-import { isSuccess, failure } from '@fracture/parse';
+import { Route, Request } from '@fracture/serve';
+import { Result, isSuccess, failure } from '@fracture/parse';
 
 type RouteType<T> = T extends Route<infer U> ? U : never;
 
-export function matchRoute<T extends Route<any>, R extends Route<any>[]>(
+export function matchRoute<T extends Route<unknown>, R extends Route<unknown>[]>(
   route: T,
   ...routes: R
-): Route<RouteType<T> | RouteType<R>> {
+): Route<RouteType<T> | RouteType<R[number]>> {
   const allRoutes = [route, ...routes];
-  return async (whatever) => {
+  return async (request) => {
     for (const route of allRoutes) {
-      const match = await route(whatever);
+      const match = (await route(request)) as Result<RouteType<typeof route>, Request>;
       if (isSuccess(match)) {
         return match;
       }
     }
-    return failure(whatever, 'No match');
+    return failure(request, 'No match');
   };
 }
