082816cbb7616f5fa6e1ca08cc5693db182481ee
Beau Collins <beau@collins.pub>
2021-01-18 17:01:18 -0800

    simplify query parsing

---
+++ src/parseQuery.ts
@@ -0,0 +1,17 @@
+import { Request } from '@fracture/serve';
+import { Parser, Success, mapFailure, failure, success } from '@fracture/parse';
+import { parse } from 'querystring';
+
+export const parseQueryString: (url: string) => Success<Record<string, string | string[]>> = (url) => {
+  const index = url.indexOf('?');
+  if (index === -1) {
+    return success({});
+  }
+  return success(parse(url.slice(index + 1)));
+};
+
+export function parseQuery<T>(parser: Parser<T, Record<string, unknown>>): Parser<T, Request> {
+  return (request: Request) => {
+    return mapFailure(parser(parseQueryString(request.url).value), ({ reason }) => failure(request, reason));
+  };
+}
--- src/service/auth/index.tsx
+++ src/service/auth/index.tsx
@@ -1,35 +1,15 @@
 import React from 'react';
-import { routes, route, jsonResponse, Request } from '@fracture/serve';
-import { Parser, success, mapSuccess, mapFailure, mapResult, failure, objectOf, isString } from '@fracture/parse';
+import { routes, route } from '@fracture/serve';
+import { mapResult, objectOf, isString } from '@fracture/parse';
 import { renderView } from '../../inLayout';
 import { get, path } from '../../path';
 import { Login } from './Login';
-import { encode, parse } from 'querystring';
+import { encode } from 'querystring';
 import { Gateways } from '../../data/gateways';
 import { InvalidCode } from './InvalidCode';
-import { Readable } from 'stream';
 import { sessionTokenForAccessToken } from '../../userIdentity';
 import { emptyBody } from '../../emptyBody';
-
-const parseQueryString: Parser<Record<string, string | string[]>, Request> = ({ request }) => {
-  const url = request.url;
-  if (url == null) {
-    return success({});
-  }
-  const index = url.indexOf('?');
-  if (index === -1) {
-    return success({});
-  }
-  return success(parse(url.slice(index + 1)));
-};
-
-function parseQuery<T>(parser: Parser<T, Record<string, unknown>>): Parser<T, Request> {
-  return (request: Request) => {
-    return mapSuccess(parseQueryString(request), (o) =>
-      mapFailure(parser(o), ({ reason }) => failure(request, reason)),
-    );
-  };
-}
+import { parseQuery } from '../../parseQuery';
 
 const createAuthUrl = () =>
   `https://github.com/login/oauth/authorize?${encode({
